f181bd7489ec7bdc97c808dcb98f03e38f4d2752
Dawood Zafar <99544039+link2dawood@users.noreply.github.com>
2025-04-29 15:18:51 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -41,31 +41,6 @@ Route::get('/terms-and-conditions', function () {
     return view('terms-and-conditions');
 });
 
-// tools start from here
-Route::get('/tools/cgpa-to-percentage', function () {
-    return view('tools.cgpa-to-percentage');
-});
-Route::get('/tools/sgpa-to-cgpa', function () {
-    return view('tools.sgpa-to-cgpa');
-});
-Route::get('/tools/marks-percentage-calculator', function () {
-    return view('tools.marks-percentage-calculator');
-});
-Route::get('/tools/percentage-to-cgpa', function () {
-    return view('tools.percentage-to-cgpa');
-});
-Route::get('/tools/sgpa-to-percentage', function () {
-    return view('tools.sgpa-to-percentage');
-});
-Route::get('/tools/percentage-to-sgpa', function () {
-    return view('tools.percentage-to-sgpa');
-});
-Route::get('/tools/final-grade', function () {
-    return view('tools.final-grade');
-});
-//tools end here 
-
-Route::get('/tools', [App\Http\Controllers\HomeController::class, 'tools'])->name('tools');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
